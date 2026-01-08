4ebaf74b8b2005e07b91b2106919eb4d29687d71
otabek0311 <abdullayevotabek414@gmail.com>
2026-01-08 22:47:42 +0500

    test

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "postgres",
+        host: config.get("DB_HOST"),
+        port: Number(config.get("DB_PORT")),
+        username: config.get("DB_USERNAME"),
+        password: config.get("DB_PASSWORD"),
+        database: config.get("DB_NAME"),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
+    AuthModule,
+  ],
+})
+export class AppModule {}
