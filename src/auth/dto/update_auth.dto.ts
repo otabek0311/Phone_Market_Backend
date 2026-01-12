e49266c2624e5d240a900ffb0742103edd56787e
otabek0311 <abdullayevotabek414@gmail.com>
2026-01-12 21:10:10 +0500

    docker

--- src/auth/dto/update_auth.dto.ts
+++ src/auth/dto/update_auth.dto.ts
@@ -1,13 +1,13 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class UpdateAuthDto {
- @ApiProperty({example: `user@mail.com`})
- @IsEmail()
- email: string; 
- 
- @ApiProperty({example: `123456`})
- @IsString()
- @MinLength(6)
- password:string
+  @ApiProperty({ example: `user@mail.com` })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: `123456` })
+  @IsString()
+  @MinLength(6)
+  password: string;
 }
