0533bffa49cc017e289edec2b27a96d490c50a1b
otabek0311 <abdullayevotabek414@gmail.com>
2026-01-20 13:16:31 +0500

    cart-entity

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -1,1 +1,29 @@
-export class Cart {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+
+import { Profile } from '../../profile/entities/profile.entity';
+import { Product } from '../../product/entities/product.entity';
+
+@Entity('cart')
+export class Cart {
+
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => Profile, { onDelete: 'CASCADE' })
+  user!: Profile;
+
+  @ManyToOne(() => Product, { eager: true, onDelete: 'CASCADE' })
+  product!: Product;
+
+  @Column({ type: 'int' })
+  quantity!: number;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
